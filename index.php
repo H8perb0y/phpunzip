c00888c9db7be9590064b49809458b2fec4fe20b
Philip <phil@hackerspace22.ml>
2015-09-13 11:46:40 +0200

    Create index.php
    Main file of unzip

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        <title>PHP-Unzip</title>
+    
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    
+        <style>
+            body {
+                font-family: 'Ubuntu', sans-serif;
+                background-color: #141313;
+                color: #FFFFFF;
+                text-shadow: 0px 3px 0px rgba(145, 143, 145, 1);
+            }
+        
+            .btn {
+              -webkit-border-radius: 9;
+              -moz-border-radius: 9;
+              border-radius: 9px;
+              text-shadow: 0px 2px 0px #918f91;
+              -webkit-box-shadow: 0px 6px 0px #006ec2;
+              -moz-box-shadow: 0px 6px 0px #006ec2;
+              box-shadow: 0px 6px 0px #006ec2;
+              color: #ffffff;
+              font-size: 34px;
+              background: #3498db;
+              padding: 8px 20px 10px 20px;
+              text-decoration: none;
+            }
+            
+            .btn:hover {
+              background: #3cb0fd;
+              background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+              background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+              background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+              background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+              background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+              text-decoration: none;
+            }
+        </style>
+    
+    </head>
+
+<body>
+
+<center>
+    
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <h1 style="font-size: 4em;">PHP-Unzip</h1>
+
+    <br />
+    <br />
+    <br />
+
+    <a class="btn" href="unzip.php">Unzip!</a>
+
+</center>
+
+</body>
+
+</html>
